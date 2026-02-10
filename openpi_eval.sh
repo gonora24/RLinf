660ac49cf735d5a1f74f5d132c13f70e9faa6cbf
student_pool <pascal.siekiera@gmail.com>
2026-02-10 11:16:33 +0100

    testing

---
+++ openpi_eval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=10
+#SBATCH --gres=gpu:1 
+#SBATCH --job-name=openpi_eval
+
+module load devel/miniforge
+module load devel/cuda
+
+source openpi-venv/bin/activate
+
+
+bash examples/embodiment/eval_embodiment.sh libero_goal_grpo_openpi
