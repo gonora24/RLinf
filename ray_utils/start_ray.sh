e3d987f1ce5a1a7bb8c18c7a1ae950e09cf47780
gonora24 <noragorhan@gmail.com>
2026-02-24 07:46:35 +0100

    Changed dashboard ip to be able to debug

--- ray_utils/start_ray.sh
+++ ray_utils/start_ray.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export RANK=0
 # Parameter check
 if [ -z "$RANK" ]; then
     echo "Error: RANK environment variable not set!"
@@ -11,6 +12,7 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_PATH=$(dirname "$SCRIPT_PATH")
 RAY_HEAD_IP_FILE=$REPO_PATH/ray_utils/ray_head_ip.txt
 RAY_PORT=${RAY_PORT:-29500}  # Default port for Ray, can be modified if needed
+export RAY_DEBUG=1
 
 # Head node startup logic
 if [ "$RANK" -eq 0 ]; then
@@ -18,7 +20,7 @@ if [ "$RANK" -eq 0 ]; then
     IP_ADDRESS=$(hostname -I | awk '{print $1}')
     # Start Ray head node
     echo "Starting Ray head node on rank 0, IP: $IP_ADDRESS"
-    ray start --head --memory=461708984320 --port=$RAY_PORT
+    ray start --head --memory=461708984320 --port=$RAY_PORT --dashboard-host $IP_ADDRESS
     
     # Write IP to file
     echo "$IP_ADDRESS" > $RAY_HEAD_IP_FILE
@@ -44,5 +46,5 @@ else
     fi
     
     echo "Starting Ray worker node connecting to head at $HEAD_ADDRESS"
-    ray start --memory=461708984320 --address="$HEAD_ADDRESS:$RAY_PORT"
+    ray start --memory=461708984320 --address="$HEAD_ADDRESS:$RAY_PORT" --dashboard-host $HEAD_ADDRESS
 fi
