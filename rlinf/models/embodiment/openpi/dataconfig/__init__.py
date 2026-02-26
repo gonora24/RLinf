ca1b4d078ca4789d31d4d7d70d4a24ce4a4ed94a
gonora24 <noragorhan@gmail.com>
2026-02-26 16:24:14 +0100

    added pi dsrl train config

--- rlinf/models/embodiment/openpi/dataconfig/__init__.py
+++ rlinf/models/embodiment/openpi/dataconfig/__init__.py
@@ -95,6 +95,27 @@ _CONFIGS = [
         pytorch_weight_path="checkpoints/torch/pi05_base",
     ),
     TrainConfig(
+        name="pi05_libero_dsrl",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=50, discrete_state_input=False),
+        data=LeRobotLiberoDataConfig(
+            repo_id="physical-intelligence/libero",
+            base_config=DataConfig(prompt_from_task=True),
+            extra_delta_transform=False,
+        ),
+        batch_size=256,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            warmup_steps=10_000,
+            peak_lr=5e-5,
+            decay_steps=1_000_000,
+            decay_lr=5e-5,
+        ),
+        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
+        ema_decay=0.999,
+        weight_loader=weight_loaders.CheckpointWeightLoader("checkpoints/jax/pi05_base"),
+        pytorch_weight_path="checkpoints/torch/pi05_base",
+        num_train_steps=30_000, 
+    ),
+    TrainConfig(
         name="pi0_maniskill",
         model=pi0_config.Pi0Config(),
         data=LeRobotManiSkillDataConfig(
