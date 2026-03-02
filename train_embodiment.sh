8f4e602f6c6c15fbdbc557dbc2c3d785d4bc6bb5
gonora24 <noragorhan@gmail.com>
2026-03-02 13:46:25 +0100

    config changes

--- train_embodiment.sh
+++ train_embodiment.sh
@@ -6,11 +6,13 @@
 export WANDB_API_KEY='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'
 export WANDB_EMAIL='noragorhan@gmail.com'
 export WANDB_USERNAME='noragorhan'
-export WANDB_TEAM='noragorhan-karlsruhe-institute-of-technology'
+export WANDB_TEAM='noras-masterarbeit'
+
+export DEBUG=0
 
 module load devel/miniforge
 module load devel/cuda
 
 source openpi-venv/bin/activate
 
-bash examples/embodiment/run_embodiment.sh libero_goal_ppo_openpi_pi05
+bash examples/embodiment/run_embodiment.sh libero_90_noise_sac_openpi05 #maniskill_ppo_openpi_pi05 #libero_goal_ppo_openpi_pi05_resume
