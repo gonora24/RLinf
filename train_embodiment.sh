44c6cd296e91fa1d4b18dfc9d6cc9aa049f7d95e
gonora24 <noragorhan@gmail.com>
2026-03-30 17:48:23 +0200

    updated

--- train_embodiment.sh
+++ train_embodiment.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1 
-#SBATCH --job-name=dsrl_spatial_train
+#SBATCH --job-name=spatial_dsrl_openpi
 
 export WANDB_API_KEY='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'
 export WANDB_EMAIL='noragorhan@gmail.com'
@@ -29,6 +29,4 @@ module load devel/cuda
 
 source openpi-venv/bin/activate
 
-bash examples/embodiment/run_embodiment.sh libero_spatial_dsrl_openpi  # hier die korrekte config angeben
-# bash examples/embodiment/run_embodiment.sh libero_object_dsrl_openpi_pi05
-# bash examples/embodiment/run_embodiment.sh libero_spatial_dsrl_openpi_pi05
+bash examples/embodiment/run_embodiment.sh libero_object_dsrl_openpi_pi05
